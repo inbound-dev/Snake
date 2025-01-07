e046cfb1014aa01a075ed13b1c8a42d7fb87560f
Jordan Hamilton <jordanh6489@hotmail.com>
2025-01-07 05:30:04 -0500

    updates

--- Snake/src/snake/Snake.java
+++ Snake/src/snake/Snake.java
@@ -4,6 +4,7 @@ import Classes.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.Graphics;
 /**
  *
  * @author Jordan H.
@@ -13,10 +14,15 @@ public class Snake extends KeyAdapter{
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Dimension size = Toolkit.getDefaultToolkit().getScreenSize(); 
+    public static void main(String[] args) {        
         //DO NOT CHANGE WINDOW SIZE
-        int windowSize = 599;
+        int windowSize = 600;
+        
+        int unitSize = 25;
+        int gameUnits = windowSize/unitSize;
+        int x[] = new int[unitSize];
+        int y[] = new int[unitSize];
+    
         
         System.out.println("Snake!");
         System.out.println("By: Jordan H");
@@ -29,8 +35,22 @@ public class Snake extends KeyAdapter{
         board.getContentPane().setBackground(Color.black);
         board.setResizable(false);
         board.setLocationRelativeTo(null);
+        board.setSize(windowSize, windowSize);
         
         // fill game board
-        GameFunctions.DrawBoard();
+        DrawBoard(gameUnits);
+        
     }
+    
+    public static void DrawBoard(int gameUnits){
+        
+            for(int i = 0; i <= gameUnits; i++){
+            
+            }
+        }
+    
+        public static void CreateApple(){
+        
+        }
+    
 }
